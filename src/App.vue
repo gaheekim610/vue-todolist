94be8dd0a7f57813ef8b10ddbf344f8b8898cbea
GAHEEKIM <gaheekim610@gmail.com>
2022-05-15 10:45:24 +0900

    전체컴포넌트 틀 구축

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <!-- custom element는 하이픈으로 연결된 소문자로 표현한다 -->
+    <TodoHeader name="Todo"></TodoHeader>
+    <todo-input></todo-input>
+    <TodoList></TodoList>
   </div>
 </template>
 
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name:'App', //todo: name을 넣어야 하는 경우는 언제인지?
+  components:{
+    TodoHeader,
+    TodoInput,
+    TodoList,
+  },
+  data() {
+    return {
+      todos:[]
+    }
+  },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="inputContainer">
+    <v-text-field
+      label="write your todo"
+      :rules="rules"
+      :todo="todo"
+      hide-details="auto"
+    >{{ todo }} </v-text-field>
+      <v-btn depressed>
+      Normal
+    </v-btn>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruels:[
+        value => !!value || 'Required'
+      ],
+      todo: "test"
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .inputContainer {
+    display: flex;
+  }
+</style>
